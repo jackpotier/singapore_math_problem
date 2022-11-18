a3cbbf1476ac4dc56e7c69a91ed8234b5ac6a8cc
Jack Potier <jackpotierschool@gmail.com>
2022-11-18 20:17:19 +1300

    solved :)

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-const finalTerm = prompt("Give the final term of the series");
+let finalTerm = prompt("Give the final term of the series");
 finalTerm = Number(finalTerm);
+let sum = 0;
+
+for (let denomenator=1; denomenator<=finalTerm; denomenator++){
+    let numIncrease = 1;
+    while (numIncrease<=denomenator){
+        let difference = denomenator-numIncrease+1;
+        sum=sum+(difference/denomenator);
+        numIncrease+=1;
+    }
+}
+
+console.log(sum);
